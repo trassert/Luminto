f34fa7124c5d4d4ec6bff6d7a2454de3f51a0e61
trassert <trassert86@gmail.com>
2026-06-08 21:20:49 +0300

    Исправление одновременного вызова функции закрытия топика и изменения её icon_id

--- modules/telegram/forum.py
+++ modules/telegram/forum.py
@@ -53,8 +53,14 @@ async def delete_topic(event: Message):
             functions.messages.EditForumTopicRequest(
                 peer=config.chats.forum,
                 topic_id=topic_id,
-                closed=True,
                 icon_emoji_id=phrase.forum.done_id,
+            )
+        )
+        await client(
+            functions.messages.EditForumTopicRequest(
+                peer=config.chats.forum,
+                topic_id=topic_id,
+                closed=True,
             )
         )
     except BadRequestError as e:
