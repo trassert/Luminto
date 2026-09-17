7e5e6d085b40e5bc7289f5540ea9092661fb49ae
trassert <trassert86@gmail.com>
2026-09-17 21:25:34 +0300

    Фикс некорректной выборки

--- modules/telegram/base.py
+++ modules/telegram/base.py
@@ -902,7 +902,7 @@ async def check_host(event: Message) -> Message:
     for node, r in data["result"].items():
         res = r[0]
         if res[0] == 1:
-            text.append(f"{func.cc_to_flag([node][0])} : {node} - ✅ - Пинг {round(res[1]*1000)} мс\n")
+            text.append(f"{func.cc_to_flag(data["info"]["nodes"][node][0])} : {node} - ✅ - Пинг {round(res[1]*1000)} мс\n")
         else:
-            text.append(f"{func.cc_to_flag([node][0])} : {node} - ❌ - {res[2]}\n")
+            text.append(f"{func.cc_to_flag(data["info"]["nodes"][node][0])} : {node} - ❌ - {res[2]}\n")
     return await event.reply("\n".join(text))
